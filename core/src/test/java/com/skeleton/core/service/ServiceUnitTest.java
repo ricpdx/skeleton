32bd3c6e36b1abaaf65af502845cf2f6e2ddcc89
Ric <ric@brinydeep.net>
2013-10-26 10:08:56 -0700

    add unit test

---
+++ core/src/test/java/com/skeleton/core/service/ServiceUnitTest.java
@@ -0,0 +1,35 @@
+package com.skeleton.core.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
+
+public class ServiceUnitTest {
+	Logger logger;
+
+	@BeforeClass
+	public void oneTimeSetup() {
+		logger = LoggerFactory.getLogger(ServiceUnitTest.class);
+		logger.info("ServiceUnitTest - oneTimeSetup");
+	}
+
+	@AfterClass
+	public void oneTimeTeardown() {
+		logger.info("ServiceUnitTest - oneTimeTeardown");
+		logger = null;
+	}
+
+	@Test
+	public void FakeTest() {
+		assertEquals(1, 2);
+		assertEquals(true, false);
+		Object x = null;
+		assertNotNull(x);
+		logger.info("ServiceUnitTest - running FakeTest");
+	}
+}
