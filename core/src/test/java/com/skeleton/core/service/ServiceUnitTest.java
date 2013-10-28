7e662f1cc1ce95def460f17e1133d5de12e05da6
Ric <ric@brinydeep.net>
2013-10-28 13:47:13 -0700

    removing integration tests

--- core/src/test/java/com/skeleton/core/service/ServiceUnitTest.java
+++ core/src/test/java/com/skeleton/core/service/ServiceUnitTest.java
@@ -6,6 +6,10 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
 
@@ -26,7 +30,23 @@ public class ServiceUnitTest {
 
 	@Test
 	public void FakeTest() {
-		assertEquals(1, 1);
 		logger.info("ServiceUnitTest - running FakeTest");
+
+		Path path = Paths.get("core/build/resources/test/test.json");
+		assertNotNull(path);
+		String dataString = readJsonFileToString(path);
+		assertNotNull(dataString);
+	}
+
+	public static String readJsonFileToString(Path path) {
+		try {
+			byte[] bytes = Files.readAllBytes(path);
+			String data = new String(bytes);
+			return data.replace("\r", "").replace("\t", " ");
+		} catch (Throwable e) {
+			e.printStackTrace();
+			System.err.println(e.getMessage());
+			return null;
+		}
 	}
 }
